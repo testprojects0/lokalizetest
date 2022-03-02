638337eb11e1211d60d0cf6155cb91df87929f4d
leonid.ivanov <leonid.ivanov@ironwaterstudio.com>
2022-03-02 18:25:30 +0300

    Lokalize SDK (part 1)

---
+++ app/src/main/java/com/example/lokalizetest/ApplicationEx.kt
@@ -0,0 +1,19 @@
+package com.example.lokalizetest
+
+import android.app.Application
+import com.lokalise.sdk.Lokalise
+
+class ApplicationEx : Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+		Lokalise.init(this, SDK_TOKEN, PROJECT_ID)
+		Lokalise.isPreRelease = true
+		Lokalise.updateTranslations()
+	}
+
+	companion object {
+		private const val SDK_TOKEN = "126c041e1d74355e5f8674b4f92c4a7de03741d9"
+		private const val PROJECT_ID = "83086131621f692de6d747.58685353"
+	}
+}
