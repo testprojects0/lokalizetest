87a123180a2abd74c40b5aab27c71637bb23c2ae
leonid.ivanov <leonid.ivanov@ironwaterstudio.com>
2022-03-03 16:26:52 +0300

    commit

--- app/src/main/java/com/example/lokalizetest/MainActivity.kt
+++ app/src/main/java/com/example/lokalizetest/MainActivity.kt
@@ -9,9 +9,7 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-		findViewById<AppCompatTextView>(R.id.tv_1).setText(R.string.app_name)
-		findViewById<AppCompatTextView>(R.id.tv_2).setText(R.string.hello_world)
-		findViewById<AppCompatTextView>(R.id.tv_3).text = LokaliseResources(this).getString(R.string.cabinet)
-		findViewById<AppCompatTextView>(R.id.tv_4).setText(R.string.cabinet)
+		findViewById<AppCompatTextView>(R.id.tv_5).setText(R.string.cabinet)
+		findViewById<AppCompatTextView>(R.id.tv_7).text = LokaliseResources(this).getString(R.string.cabinet)
 	}
 }
